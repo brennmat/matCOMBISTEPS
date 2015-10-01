71dabc38181c06c59d018fbe32b09bc1c210c0a0
brennmat <brennmat@497d688e-ddd5-4961-ab03-f540487850d5>
2015-10-01 06:36:26 +0000

    fixed but with displaying lab codes / samples in run overview.

--- tools/matCS_run_print_summary.m
+++ tools/matCS_run_print_summary.m
@@ -13,15 +13,15 @@ function run = matCS_run_print_summary (run)
 fflush (stdout);
 disp (sprintf("Number of steps: %i",length(run.steps)))
 for i = 1:length(run.steps)
-%    info = sprintf("Entry %i: Machine = %s, step number = %i, file = %s, type = %s, run = %s, date = %s, manual dilution = %g", i, run.steps(i).machine, run.steps(i).number, run.steps(i).file, run.steps(i).type, run.steps(i).run, run.steps(i).date, run.steps(i).manual_dilution);
-    info = sprintf("%i: Machine = %s, step = %i, file = %s, type = %s, run = %s, date = %s, manual dilution = %g", i, run.steps(i).machine, run.steps(i).number, run.steps(i).file, run.steps(i).type, run.steps(i).run, run.steps(i).date, run.steps(i).manual_dilution);
+%    L = sprintf("Entry %i: Machine = %s, step number = %i, file = %s, type = %s, run = %s, date = %s, manual dilution = %g", i, run.steps(i).machine, run.steps(i).number, run.steps(i).file, run.steps(i).type, run.steps(i).run, run.steps(i).date, run.steps(i).manual_dilution);
+    L = sprintf("%i: Machine = %s, step = %i, file = %s, type = %s, run = %s, date = %s, manual dilution = %g", i, run.steps(i).machine, run.steps(i).number, run.steps(i).file, run.steps(i).type, run.steps(i).run, run.steps(i).date, run.steps(i).manual_dilution);
     switch upper(run.steps(i).type)
         case "C"
-            info = sprintf ("%s, bottle = %s, dilution = %g", info, run.steps(i).bottle, run.steps(i).dilution);
+            L = sprintf ("%s, bottle = %s, dilution = %g", L, run.steps(i).bottle, run.steps(i).dilution);
         case "S"
-	    info = sprintf ("%s, lab code = %s", info,  matCS_step_labcode(run.steps(i)));
+	    	L = sprintf ("%s, lab code = %s", L,  matCS_step_labcode(run.steps(i)){});
     end
-    disp (info);
+    disp (L);
     fflush (stdout);
 end
 end
